1dd961ec7ea9648ab711251b08f43a77614e37ce
Helen Poe <helenpoe@MacBook-Air-Helen.local>
2022-04-07 18:23:46 +0300

    initial commit

---
+++ myExpensesSwiftUI/myExpensesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  myExpensesSwiftUIApp.swift
+//  myExpensesSwiftUI
+//
+//  Created by Helen Poe on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct myExpensesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
